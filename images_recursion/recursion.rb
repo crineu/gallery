f893cdaad188a2683a11af8fff9a2e0794c20431
Crineu Tres <crineu.tres@ufsc.br>
2013-02-07 13:53:12 -0200

    Finished recursion problem

--- images_recursion/recursion.rb
+++ images_recursion/recursion.rb
@@ -4,26 +4,43 @@ def size(image_filename)
 		return IO.read(image_filename)[0x10..0x18].unpack('NN')
 	elsif '.gif' ==  File.extname(image_filename)
 		return IO.read(image_filename)[6..10].unpack('SS')
+	else
+		return [10, 10]
 	end
 end
 
 # struct FTW!
 class Image
 	attr_accessor :height, :width
-	def initialize (array)
+	def initialize (array) 
 		@height, @width = array.first, array.last
+	end
+	def to_s
+		"#{@height}x#{@width}"
 	end
 end
 
 result = []
 folders = ['.'] + Dir.glob("**/*/")
 folders.each do |folder|
-	r_folders = []
+	heights, widths = [], []
 	files = Dir.entries(folder)
 	files.reject! { |file| not ['.png', '.gif'].include? File.extname(file)  }
+	files.map! { |file| folder + file } unless folder == '.'
 	files.each do |filename|
-		img = Image.new(size(folder + filename))
-		r_folders << img
+		img = Image.new(size(filename))
+		heights << img.height
+		widths  << img.width
 	end
-	p r_folders
+	
+	unless heights.empty?
+		total_h = heights.reduce(:+)
+		total_w = widths.reduce(:+)
+		puts "*********"
+		puts " Information for folder " + folder
+		puts " Number of images: " + heights.size.to_s
+		puts " Total / average height:   " + total_h.to_s + " / " + (total_h.to_f / heights.size).to_s
+		puts " Total / average width:    " + total_w.to_s + " / " + (total_w.to_f / widths.size).to_s
+		puts ""
+	end
 end
