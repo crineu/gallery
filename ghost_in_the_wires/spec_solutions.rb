0eb8ec78ac3bd5fe6c54a8dbe919612a24b289ee
Crineu Tres <crineu.tres@ufsc.br>
2014-05-19 17:40:49 -0300

    Ever\!

--- ghost_in_the_wires/spec_solutions.rb
+++ ghost_in_the_wires/spec_solutions.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
-require File.dirname(__FILE__) + '/ghost.rb'
-
+require_relative 'ghost.rb'
 
 def file_to_array(file_name)\
 	array = []
@@ -11,64 +10,28 @@ def file_to_array(file_name)\
 	array
 end
 
-describe "Tests for all chapters solutions" do
+describe "Test all chapters solutions" do
 
 	before do 
 		CODES   ||= file_to_array('chapter_codes')
 		RIDDLES ||= file_to_array('chapter_riddles')
-		ANSWERS ||= file_to_array('chapter_answers')		
+		ANSWERS ||= file_to_array('chapter_answers')
 	end
 	
 	describe "most of the odd chapters use ROT" do
-		it "chapter 1 is decyphered with index 2" do
-			CODES[1].rot(2).must_equal RIDDLES[1]
-		end
-		it "chapter 3 is decyphered with index 17" do
-			CODES[3].rot(17).must_equal RIDDLES[3]
-		end
-		it "chapter 5 is decyphered with index 5" do
-			CODES[5].rot(5).must_equal RIDDLES[5]
-		end
-		it "chapter 7 is decyphered with index 14" do
-			CODES[7].rot(14).must_equal RIDDLES[7]
-		end
-		it "chapter 9 is decyphered with index 11" do
-			CODES[9].rot(11).must_equal RIDDLES[9]
-		end
-		it "chapter 11 is decyphered with index 15" do
-			CODES[11].rot(15).must_equal RIDDLES[11]
-		end
-		it "chapter 13 is decyphered with index 3" do
-			CODES[13].rot(3).must_equal RIDDLES[13]
-		end
-		it "chapter 15 is decyphered with index 12" do
-			CODES[15].rot(12).must_equal RIDDLES[15]
-		end
-		it "chapter 17 is decyphered with index 8" do
-			CODES[17].rot(8).must_equal RIDDLES[17]
-		end
-		it "chapter 19 is decyphered with index 21" do
-			CODES[19].rot(21).must_equal RIDDLES[19]
-		end
-		it "chapter 23 is decyphered with index 9" do
-			CODES[23].rot(9).must_equal RIDDLES[23]
-		end
-		it "chapter 25 is decyphered with index 6" do
-			CODES[25].rot(6).must_equal RIDDLES[25]
-		end
-		it "chapter 31 is decyphered with index 4" do
-			CODES[31].rot(4).must_equal RIDDLES[31]
-		end
-		it "chapter 33 is decyphered with index 18" do
-			CODES[33].rot(18).must_equal RIDDLES[33]
+		{1=>2, 3=>17, 5=>5, 7=>14, 9=>11, 11=>15, 13=>3, 15=>12,
+		 17=>8, 19=>21, 23=>9, 25=>6, 31=>4, 33=>18}.each_pair do |chapter, key|		
+			it "chapter #{chapter} is decyphered using key #{key}" do
+				CODES[chapter].rot(key).must_equal RIDDLES[chapter]
+			end
 		end
 	end
 
-	describe "all even chapters use vigenere" do
+	describe "even chapters use vigenere cypher" do
 		2.step(37, 2).each do |chapter|
-			it "uses previous chapters answers as key" do
-				previos_chapter_answer = ANSWERS[chapter - 1]
-				CODES[chapter].vigenere(previos_chapter_answer).must_equal RIDDLES[chapter]
+			it "chapter #{chapter} uses previous chapter (#{chapter-1}) answers as key" do
+				previous_chapter_answer = ANSWERS[chapter - 1]
+				CODES[chapter].vigenere(previous_chapter_answer).must_equal RIDDLES[chapter]
 			end
 		end
 	end
