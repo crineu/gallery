c48ca2cf1ec975d76fc3434cc6cba73585e27089
Crineu Tres <crineu@gmail.com>
2018-09-10 22:16:49 -0300

    Elixir Issues: First commit of the 'issue' project

---
+++ elixir/issues/lib/issues/cli.ex
@@ -0,0 +1,37 @@
+defmodule CLI do
+
+  @default_count 5
+
+  @moduledoc """
+  Hanlde the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can be -h or --help, witch returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, strict:  [help: :boolean],
+                                     aliases: [   h: :help   ])
+    case parse do
+      { [help: true], _, _}
+        -> :help
+      { _, [user, project, count], _ }
+        -> { user, project, count }
+      { _, [user, project], _ }
+        -> { user, project, @default_count }
+      _ -> :help
+    end
+  end
+
+end
