6bb38d82881b7bf6eafce90684696ecaf516ba61
Crineu Tres <crineu@gmail.com>
2018-09-11 23:38:57 -0300

    Elixir Issues: added Log; project functional

--- elixir/issues/lib/issues/cli.ex
+++ elixir/issues/lib/issues/cli.ex
@@ -3,7 +3,7 @@ defmodule Issues.CLI do
   @default_count 5
 
   @moduledoc """
-  Hanlde the command line parsing and the dispatch to
+  Handle the command line parsing and the dispatch to
   the various functions that end up generating a
   table of the last _n_ issues in a github project
   """
--- elixir/issues/mix.exs
+++ elixir/issues/mix.exs
@@ -3,10 +3,12 @@ defmodule Issues.MixProject do
 
   def project do
     [
-      app:     :issues,
-      escript: escript_config(),
-      version: "0.1.0",
-      elixir:  "~> 1.7",
+      app:        :issues,
+      name:       "Issues",
+      source_url: "http://github.com/bozo/nada",
+      escript:    escript_config(),
+      version:    "0.1.0",
+      elixir:     "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,7 +27,8 @@ defmodule Issues.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       { :httpoison, "~> 1.0" },
-      { :poison,    "~> 3.1" }
+      { :poison,    "~> 3.1" },
+      { :ex_doc,    "~> 0.19", only: :dev, runtime: false}
     ]
   end
 
