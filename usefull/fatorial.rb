30b74c3961f0bbab917b2c42e75f945f3b249ff7
Crineu Tres <crineu@gmail.com>
2019-09-13 00:11:24 -0300

    Fixing identation

--- usefull/fatorial.rb
+++ usefull/fatorial.rb
@@ -3,28 +3,28 @@ require 'benchmark'
 
 # Common way of doing fatorial with while
 def fatorial_while(num)
-    result = 1
-    while (num > 1)
-        result *= num
-        num = num -1
-    end
-    result
+	result = 1
+	while (num > 1)
+		result *= num
+		num = num -1
+	end
+	result
 end
 
 # Recursive fatorial implementation
 def fatorial_recursivo(num)
-    return num == 1 ? 1 : num * fatorial_recursivo(num-1)
+	return num == 1 ? 1 : num * fatorial_recursivo(num-1)
 end
 
 # Fatorial with ruby inject method
 def fatorial_inject(num)
-    (2..num).inject { |x, num| x *= num }
+	(2..num).inject { |x, num| x *= num }
 end
 
 
-numero = 8100
+numero = 9400
 
-REPETICOES = 98
+REPETICOES = 99
 LARGURA_COLUNA = 16
 Benchmark.bm(LARGURA_COLUNA) do |x|
   x.report("Using while")       { REPETICOES.times { fatorial_while(numero) } }
